6a9436d6740d50e0f7a0d8f2c0150df1c4bff276
Alejandro1267 <164577387+Alejandro1267@users.noreply.github.com>
2025-07-28 15:14:11 -0700

    Agregó AccountsList

--- app/(tabs)/cuentas.tsx
+++ app/(tabs)/cuentas.tsx
@@ -1,14 +1,11 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 
 import AccountModal from "@/components/finance/AccountModal";
+import { AccountsList } from "@/components/finance/AccountsList";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { IconSymbol } from "@/components/ui/IconSymbol";
-import { useFinanceStore } from "@/store/FinanceStore";
 
 export default function TabTwoScreen() {
-  const { accounts, setShowAccountModal, createEmptyAccount } =
-    useFinanceStore();
-
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#C8E6C9", dark: "#237D32" }}
@@ -21,28 +18,7 @@ export default function TabTwoScreen() {
         />
       }
     >
-      <ScrollView style={styles.titleContainer}>
-        <View>
-          {/* <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              setShowAccountModal(true);
-              createEmptyAccount();
-            }}
-          >
-            <Text style={styles.buttonText}>Agregar Cuenta</Text>
-          </TouchableOpacity> */}
-
-          <View style={styles.list}>
-            {accounts.map((account) => (
-              <View key={account.name}>
-                <Text style={styles.listItem}>{account.name}</Text>
-              </View>
-            ))}
-          </View>
-        </View>
-      </ScrollView>
-
+      <AccountsList />
       <AccountModal />
     </ParallaxScrollView>
   );
@@ -53,27 +29,5 @@ const styles = StyleSheet.create({
     bottom: -90,
     left: -35,
     position: "absolute",
-  },
-  titleContainer: {
-    flexDirection: "row",
-    gap: 32,
-  },
-  button: {
-    backgroundColor: "#4CAF50",
-    padding: 16,
-    borderRadius: 8,
-  },
-  buttonText: {
-    color: "#fff",
-    fontWeight: "bold",
-    fontSize: 16,
-  },
-  list: {
-    flexDirection: "column",
-    // gap: 24,
-  },
-  listItem: {
-    // backgroundColor: "#FF6F00",
-    padding: 8,
   },
 });
