3eeb1642c52fe07898f1fb470c8f1187799f40cf
Alejandro1267 <164577387+Alejandro1267@users.noreply.github.com>
2025-08-09 11:10:38 -0700

    Agregó transferSchema

--- schemas/index.ts
+++ schemas/index.ts
@@ -18,3 +18,13 @@ export const recordSchema = z.object({
 });
 
 export type RecordDraft = z.infer<typeof recordSchema>;
+
+export const transferSchema = z.object({
+  amount: z.number().min(0.01, "El importe debe ser mayor a 0"),
+  description: z.string(),
+  date: z.string().min(1, "La fecha es requerida"),
+  originAccount: z.string().min(1, "La cuenta origen es requerida"),
+  destinationAccount: z.string().min(1, "La cuenta destino es requerida"),
+});
+
+export type TransferDraft = z.infer<typeof transferSchema>;
