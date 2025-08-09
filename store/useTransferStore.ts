ca4f22b785601b74f98a6094e6e8d8016b764478
Alejandro1267 <164577387+Alejandro1267@users.noreply.github.com>
2025-08-09 09:30:11 -0700

    useTransferStore

---
+++ store/useTransferStore.ts
@@ -0,0 +1,85 @@
+import { fechaLocal } from "@/helpers"
+import { ValidationErrors } from "@/types"
+import { create } from "zustand"
+
+export type Transfer = {
+  id: string
+  date: string
+  amount: number
+  description: string
+  originAccount: string
+  destinationAccount: string
+}
+
+type TransferState = {
+  // State
+  currentTransfer: Transfer | null
+  transfers: Transfer[]
+  transferErrors: ValidationErrors
+  transferMode: "edit" | "new"
+
+  // Actions
+  setCurrentTransfer: (transfer: Transfer | null) => void
+  setTransfers: (transfers: Transfer[]) => void
+  setTransferErrors: (errors: ValidationErrors) => void
+  setTransferField: <K extends keyof Transfer>(field: K, value: Transfer[K]) => void
+  setTransferMode: (mode: "edit" | "new") => void
+
+  // Functions
+  addTransfer: (transfer: Transfer) => void
+  createEmptyTransfer: () => void
+  clearTransferErrors: () => void
+
+}
+
+export const useTransferStore = create<TransferState>((set, get) => ({
+  // Initial State
+  currentTransfer: null,
+  transfers: [],
+  transferErrors: {},
+  transferMode: "new",
+
+  // Actions
+  setCurrentTransfer: (transfer: Transfer | null) => {
+    set({ currentTransfer: transfer })
+  },
+  setTransfers: (transfers: Transfer[]) => {
+    set({ transfers })
+  },
+  setTransferErrors: (errors: ValidationErrors) => {
+    set({ transferErrors: errors })
+  },
+  setTransferField: <K extends keyof Transfer>(field: K, value: Transfer[K]) =>
+    set((state) => {
+      if (!state.currentTransfer) return state
+      return {
+        ...state,
+        currentTransfer: {
+          ...state.currentTransfer,
+          [field]: value,
+        },
+      }
+    }),
+  setTransferMode: (mode: "edit" | "new") => {
+    set({ transferMode: mode })
+  },
+  
+  // Functions
+  addTransfer: (transfer: Transfer) => {
+    set({ transfers: [transfer, ...get().transfers] })
+  },
+  createEmptyTransfer: () => {
+    set({ currentTransfer: {
+      id: "",
+      date: fechaLocal(), // YYYY-MM-DD
+      amount: 0,
+      description: "",
+      originAccount: "",
+      destinationAccount: ""
+    }})
+  },
+  clearTransferErrors: () => {
+    set({ transferErrors: {} })
+  },
+
+}))
