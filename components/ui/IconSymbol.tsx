194be0f1c9a8b5eb00136d4e81e19ea913088938
Alejandro1267 <164577387+Alejandro1267@users.noreply.github.com>
2025-08-29 12:15:44 -0700

    Agregó ícono

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -20,6 +20,7 @@ const MAPPING = {
   "house.fill": "home",
   "paperplane.fill": "send",
   "chevron.left.forwardslash.chevron.right": "code",
+  "chevron.left": "chevron-left",
   "chevron.right": "chevron-right",
   "creditcard.fill": "wallet",
   plus: "add",
